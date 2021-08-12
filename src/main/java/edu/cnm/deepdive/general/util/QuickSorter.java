7b88ffa44c3db7c952852c01bbcc5fa3079c2d80
Nick Bennett <nick@nickbenn.com>
2021-08-12 09:52:08 -0600

    QuickSort implemented (hopefully).

---
+++ src/main/java/edu/cnm/deepdive/general/util/QuickSorter.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.general.util;
+
+import java.util.Random;
+
+public class QuickSorter {
+
+  private static Random rng = new Random();
+
+  public <T extends Comparable<? super T>> void sort(T[] data) {
+    sort(data, 0, data.length);
+  }
+
+  public <T extends Comparable<? super T>> void sort(T[] data, int start, int end) {
+    if (end > start + 1) {
+      int pivotIndex = rng.nextInt(end - start) + start;
+      T pivot = data[pivotIndex];
+      data[pivotIndex] = data[start];
+      data[start] = pivot;
+      int marker = 0;
+      for (int i = start + 1; i < end; i++) {
+        T value = data[i];
+        if (value.compareTo(pivot) <= 0) {
+          marker++;
+          if (i > marker) {
+            data[i] = data[marker];
+            data[marker] = value;
+          }
+        }
+      }
+      data[start] = data[marker];
+      data[marker] = pivot;
+      sort(data, start, marker);
+      sort(data, marker + 1, end);
+    }
+  }
+
+}
